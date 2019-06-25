aadeef49c9ede295134fe52b3983fbeb9c28091c
Yogendra Singh <yogisingh4yu@gmail.com>
2019-06-25 13:50:58 +0530

    Added Permissions in ImagePicker

--- src/UserProfileScreen.js
+++ src/UserProfileScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { View, Text, Image, Button } from 'react-native'
+import { View, Text, Image, Button, PermissionsAndroid } from 'react-native'
 import firebase from 'firebase';
 import ImagePicker from 'react-native-image-picker';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -11,49 +11,57 @@ class UserProfileScreen extends Component {
         this.state = {
             loading: false,
             modalVisible: false,
-            avatar:'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Tom_Cruise_avp_2014_4.jpg/170px-Tom_Cruise_avp_2014_4.jpg'
+            avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Tom_Cruise_avp_2014_4.jpg/170px-Tom_Cruise_avp_2014_4.jpg'
         };
     }
+
 
 
     componentDidMount() {
         const recentPostsRef = firebase.database().ref('/UsersDetail');
         recentPostsRef.once('value').then(snapshot => {
             snapshot.forEach(child => {
-                if (firebase.auth().currentUser.email == child.val().Email.toLowerCase()) {
+                if (firebase.auth().currentUser.email === child.val().Email.toLowerCase()) {
                     //listArray.push({...child.val(),key:child.key})
-                    this.userInfo = { ...child.val(), key: child.key }
+                    this.userInfo = { ...child.val(), key: child.key };
                 }
             });
-            this.setState({ loading: true })
-
-        })
-
+            this.setState({ loading: true });
+        });
     }
+
+    async requestCameraPermission() {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.CAMERA
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            console.warn('You can use the camera');
+            ImagePicker.showImagePicker(null, response => {
+                if (response.didCancel) {
+                    console.warn('Really ??');
+                } else if (response.error) {
+                    console.warn(response.error);
+                } else {
+                       this.setState({ avatar: response.uri });
+                }
+            });
+          } else {
+            console.warn('Camera permission denied');
+          }
+        } catch (err) {
+          console.warn(err);
+        }
+      }
+    
        
-    addAvatar=()=>{
-        ImagePicker.showImagePicker(null,response=>{
-            if(response.didCancel){
-                console.warn('Really ??')
-    
-            }else if(response.error){
-                console.warn(response.error)
-    
-            }else{
-                   this.setState({avatar:response.uri})
-                   
-        
-                   console.warn(this.state.avatar)
-            }
-        })
+    addAvatar= () => {
+        this.requestCameraPermission();
     }
 
-
-
-
-    render() {
-        console.warn(this.userInfo)
-        console.warn(firebase.currentUser)
+ render() {
+        console.warn(this.userInfo);
+        console.warn(firebase.currentUser);
         let username = '';
         let userEmail = '';
         let userDOB = '';
@@ -65,50 +73,41 @@ class UserProfileScreen extends Component {
         }
 
         return (
-            
-        
-            <View style={{ flex: 1 }}>
-                <View style={{ height: '35%', backgroundColor: '#ff9a3d', justifyContent: 'center', alignItems: 'center' }}>
-
-                  <View style={{flexDirection:'row'}}>
-                  <Image source={{ uri: this.state.avatar }}
-                        style={{
-                            width: 150,
-                            height: 150,
-                            borderRadius: 150 / 2,
-                            borderWidth: 3,
-                        }}
+                
+        <View style={styles.container}>
+             <View style={styles.headerContainer}>
+                <View style={styles.imageContainer}>
+                  <Image 
+                   source = {{ uri: this.state.avatar }}
+                        style={styles.imageStyle}
                         resizeMode='cover'
-
-                    />
-                    <Icon name="edit" size={20} color='white' onPress={()=>this.addAvatar()} style={{position:'absolute', bottom:16,right:7}}/>
-                  </View>
-                    <Text style={{ fontSize: 20, fontWeight: '500', marginBottom: 5, marginTop: 2 }}>Hello,{username}</Text>
+                  />
+                  <Icon 
+                     name="edit" 
+                     size={20} 
+                     color='white' 
+                     onPress={() => this.addAvatar()} 
+                     style={styles.ImageiconStyle}
+                  />
                 </View>
+                <Text style={styles.ImageTextSyle}>Hello,{username}</Text>
+              </View>
 
-                <View style={{ flex: 1, backgroundColor: '#ffaf00', justifyContent: 'space-between', padding: 10 }}>
-                    <View style={{ backgroundColor: '#ffaf5f', borderRadius: 20, padding: 10, justifyContent: 'space-around' }}>
-                        {/* <View style={{paddingVertical:15,backgroundColor:'#00d7ff',marginBottom:5,borderRadius:5,paddingLeft:5,justifyContent:'center'}}> */}
-                        <Text style={{ textAlign: 'center', fontSize: 25, color: 'white', paddingBottom: 10 }}>Personal Information</Text>
-
-                        <View style={{ paddingVertical: 15, backgroundColor: '#808080', marginBottom: 5, borderRadius: 5, paddingLeft: 5, justifyContent: 'center' }}>
-
-                            <Text style={{ fontSize: 17, fontWeight: '500', color: 'white' }}>Name:{username}</Text>
-                        </View>
-                        <View style={{ paddingVertical: 15, backgroundColor: '#808080', marginBottom: 5, borderRadius: 5, paddingLeft: 5, justifyContent: 'center' }}>
-
-
-                            <Text style={{ fontSize: 17, fontWeight: '500', color: 'white' }}>Email:{userEmail}</Text>
-                        </View>
-                        <View style={{ paddingVertical: 15, backgroundColor: '#808080', marginBottom: 5, borderRadius: 5, paddingLeft: 5, justifyContent: 'center' }}>
-
-
-                            <Text style={{ fontSize: 17, fontWeight: '500', color: 'white' }}>DOB:{userDOB}</Text>
-                        </View>
-
+              <View style={styles.informationContainer}>
+                <View style={styles.informationViewStyle}>
+                    <Text style={styles.informationHeadingStyle}>Personal Information</Text>
+                    <View style={styles.UserInformationViewStyle}>
+                        <Text style={styles.userInformatinTextStyle}>Name:{username}</Text>
                     </View>
-                </View>
-                <Button title="sign Out" onPress={async ()=>{return( await firebase.auth().signOut(),this.props.navigation.navigate('Loading'))}}/>
+                    <View style={styles.UserInformationViewStyle}>
+                        <Text style={styles.userInformatinTextStyle}>Email:{userEmail}</Text>
+                    </View>
+                    <View style={styles.UserInformationViewStyle}>
+                       <Text style={styles.userInformatinTextStyle}>DOB:{userDOB}</Text>
+                    </View>
+                 </View>
+               </View>
+              <Button title="sign Out" onPress={async ()=>{return( await firebase.auth().signOut(),this.props.navigation.navigate('Loading'))}}/>
                 
             </View>
 
@@ -116,5 +115,78 @@ class UserProfileScreen extends Component {
         );
     }
 }
+
+const styles = {
+
+    container: {
+        flex: 1
+    },
+
+    headerContainer: {
+        height: '35%', 
+        backgroundColor: '#ff9a3d', 
+        justifyContent: 'center', 
+        alignItems: 'center' 
+    },
+
+    imageContainer: {
+        flexDirection: 'row'
+    },
+    
+    imageStyle: {
+        width: 150,
+        height: 150,
+        borderRadius: 150 / 2,
+        borderWidth: 3,
+    },
+
+    ImageiconStyle: {
+        position:'absolute', 
+        bottom:16,
+        right:7
+    },
+
+    ImageTextSyle: {
+        fontSize: 20, 
+        fontWeight: '500', 
+        marginBottom: 5, 
+        marginTop: 2
+    },
+
+    informationContainer: {
+        flex: 1, 
+        backgroundColor: '#ffaf00', 
+        justifyContent: 'space-between', 
+        padding: 10 
+    },
+    
+    informationViewStyle: {
+        backgroundColor: '#ffaf5f', 
+        borderRadius: 20, 
+        padding: 10, 
+        justifyContent: 'space-around' 
+    },
+
+    informationHeadingStyle: {
+        textAlign: 'center', 
+        fontSize: 25, 
+        color: 'white', 
+        paddingBottom: 10
+    },
+
+    userInformationViewStyle: {
+        paddingVertical: 15, backgroundColor: '#808080', 
+        marginBottom: 5, 
+        borderRadius: 5, 
+        paddingLeft: 5, 
+        justifyContent: 'center' 
+    },
+    
+    userInformatinTextStyle : {
+        fontSize: 17, 
+        fontWeight: '500', 
+        color: 'white'
+    }
+};
 
 export default UserProfileScreen;
